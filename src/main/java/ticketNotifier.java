8ed3327c0076f3873ab843e27882dccea13642f1
leThrax <1852776@uni-wuppertal.de>
2022-02-09 19:46:05 +0100

    initial project commit

---
+++ src/main/java/ticketNotifier.java
@@ -0,0 +1,7 @@
+import net.dv8tion.jda.api.JDABuilder;
+
+public class ticketNotifier {
+    public void main (String [] args) {
+        //JDABuilder builder = JDABuilder.createDefault();
+    }
+}
